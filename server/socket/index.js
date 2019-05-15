bc37086f3db6ad1fd580df26510162466d00e5f6
Anna Mai <annam0403@gmail.com>
2019-05-15 08:47:14 -0400

    Add socket listeners for validating team presence

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,15 +1,34 @@
-const redisClient = require('../redis');
-const {promisify} = require('util');
-const getSetMembersAsync = promisify(redisClient.smembers).bind(redisClient);
-const addSetAsync = promisify(redisClient.sadd).bind(redisClient);
+const {
+  addToEventTeamSetAsync,
+  getSetMembersAsync,
+  addPlayerToEventTeamHashAsync,
+  deletePlayerFromEventTeamHashAsync,
+  getEventTeamHashValsAsync
+} = require('../redis');
 
-const { JOIN_TEAM_ROOM, LEAVE_TEAM_ROOM, BROADCAST_GAME_START, BROADCAST_JOINED_GAME, GAME_START, JOINED_GAME } = require('./constants');
+const {
+  JOIN_TEAM_ROOM,
+  LEAVE_TEAM_ROOM,
+  BROADCAST_GAME_START,
+  BROADCAST_JOINED_GAME,
+  GAME_START,
+  JOINED_GAME,
+  ENTER_GEOFENCE,
+  EXIT_GEOFENCE,
+  BROADCAST_TASK_COMPLETE,
+  BROADCAST_END_GAME,
+  CONFIRM_TEAM_PRESENCE,
+  COMPLETE_TASK,
+  END_GAME
+} = require('./constants');
 
 module.exports = io => {
   io.on('connection', socket => {
     let teamRoom = null;
 
-    console.log(`A socket connection to the server has been made: ${socket.id}`);
+    console.log(
+      `A socket connection to the server has been made: ${socket.id}`
+    );
 
     socket.on(JOIN_TEAM_ROOM, teamId => {
       teamRoom = teamId;
@@ -19,23 +38,54 @@ module.exports = io => {
 
     socket.on(BROADCAST_GAME_START, ({game, username}) => {
       socket.broadcast.to(teamRoom).emit(GAME_START, game);
-      addSetAsync(`eventTeam:${game.id}:players`, username);
+      addToEventTeamSetAsync(game.id, username);
     });
 
-    socket.on(BROADCAST_JOINED_GAME, ({eventTeamId, username}) => {
+    socket.on(BROADCAST_JOINED_GAME, async ({eventTeamId, username}) => {
       socket.broadcast.to(teamRoom).emit(JOINED_GAME, username);
-      addSetAsync(`eventTeam:${eventTeamId}:players`, username)
-        .then(() => getSetMembersAsync(`eventTeam:${eventTeamId}:players`)
-          .then(players => console.log(players)));
+      await addToEventTeamSetAsync(eventTeamId, username);
+      const players = await getSetMembersAsync(eventTeamId);
+      console.log(players);
     });
 
-    socket.on(LEAVE_TEAM_ROOM, (teamId) => {
+    socket.on(ENTER_GEOFENCE, async ({eventTeamId, username, geoIdentifier}) => {
+      // TODO make this multi transaction
+      await addPlayerToEventTeamHashAsync(eventTeamId, username, geoIdentifier);
+
+      // grab players from hash
+      const players = await getSetMembersAsync(eventTeamId);
+      const locations = await getEventTeamHashValsAsync(eventTeamId);
+      console.log(players, locations);
+
+      // emit event to confirm whether all players present
+      socket.emit(CONFIRM_TEAM_PRESENCE, {
+        isTeamPresent: locations.length === players.length && locations.every(location => location === geoIdentifier),
+        missingPlayerCount: players.length - locations.length,
+        taskId: geoIdentifier
+      });
+    });
+
+    socket.on(EXIT_GEOFENCE, async ({eventTeamId, username}) => {
+      await deletePlayerFromEventTeamHashAsync(eventTeamId, username);
+    });
+
+    socket.on(BROADCAST_TASK_COMPLETE, (taskId) => {
+      // send task completion message so client can dispatch
+      socket.broadcast.to(teamRoom).emit(COMPLETE_TASK, { taskId });
+    });
+
+    socket.on(BROADCAST_END_GAME, ({eventTeamId, score}) => {
+      // send end game details so other clients can dispatch
+      socket.broadcast.to(teamRoom).emit(END_GAME, score);
+    });
+
+    socket.on(LEAVE_TEAM_ROOM, teamId => {
       socket.leave(teamId);
     });
 
     socket.on('disconnect', () => {
-      console.log(`Connection ${socket.id} has left the building`)
+      console.log(`Connection ${socket.id} has left the building`);
       socket.leave(teamRoom);
     });
   });
-}
+};
